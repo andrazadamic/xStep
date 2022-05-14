0e96363a59cb2bbae9e4d16e014c75a0b00a3ac7
Andraž Adamič <ajo@s.local>
2022-05-14 15:24:54 +0200

    Scaffolding of app done.

---
+++ lib/games_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class GamesPage extends StatelessWidget {
+  const GamesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text("Games"),
+      ),
+    );
+  }
+}
---
+++ lib/leaderboard_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LeaderboardPage extends StatelessWidget {
+  const LeaderboardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text("Leaderboards"),
+      ),
+    );
+  }
+}
